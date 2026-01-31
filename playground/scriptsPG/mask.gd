c12511d9ce58a665dfbd394608bf6a553a553384
Azambuja1 <supervinho2020@gmail.com>
2026-01-31 17:16:27 -0300

    prototipo level

--- playground/scriptsPG/mask.gd
+++ playground/scriptsPG/mask.gd
@@ -3,7 +3,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	body.emit_signal("water_mask")
-	var outro_corpo = get_node("/root/Playground/Block")
-	if outro_corpo:
-		outro_corpo.emit_signal("water_mask")
 	queue_free()
