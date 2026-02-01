3acba1eff6f522a7259ce2de865daa67d121e16e
pedenrique9029 <pedrommfrv@gmail.com>
2026-02-01 01:42:59 -0300

    generic_mask  with sprites

--- generic_mask.gd
+++ generic_mask.gd
@@ -3,6 +3,11 @@ extends Node2D
 
 enum MaskElement {VENTO, FOGO, AGUA, SOMBRA}
 
+var mask_vento := preload("res://assets/masks/mask_fujin.png")
+var mask_fogo := preload("res://assets/masks/mask_kagutsuchi.png")
+var mask_escuridao := preload("res://assets/masks/mask_izanami.png")
+#var mask_vento := preload("res://assets/masks/mask_fujin.png")
+
 @export var mask_element: MaskElement:
 	set(value):
 		mask_element = value
@@ -20,12 +25,15 @@ func _update_sprite():
 	match mask_element:
 		MaskElement.VENTO:
 			sprite.modulate = Color.CYAN
+			sprite.texture = mask_vento
 		MaskElement.FOGO:
 			sprite.modulate = Color.ORANGE_RED
+			sprite.texture = mask_fogo
 		MaskElement.AGUA:
 			sprite.modulate = Color.CORNFLOWER_BLUE
 		MaskElement.SOMBRA:
 			sprite.modulate = Color.REBECCA_PURPLE
+			sprite.texture = mask_escuridao
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
